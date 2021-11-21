29120f1731195877fbfcd17a4491720da717ab17
OscarAlvarezSanchez <94078326+OscarAlvarezSanchez@users.noreply.github.com>
2021-11-21 17:16:59 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBYsPHpdYXy6jvmrRt-vNh5COOePT4GmGE",
-  authDomain: "rfbdwp801.firebaseapp.com",
-  projectId: "rfbdwp801",
-  storageBucket: "rfbdwp801.appspot.com",
-  messagingSenderId: "335302255418",
-  appId: "1:335302255418:web:15427d0d702517c426a603",
-  measurementId: "G-13RB54RPGY"
+  apiKey: "AIzaSyAti9ZfTFswxdKWQ0YMCkoRZuca4Fd_l9E",
+  authDomain: "chat801oas.firebaseapp.com",
+  projectId: "chat801oas",
+  storageBucket: "chat801oas.appspot.com",
+  messagingSenderId: "1068033436488",
+  appId: "1:1068033436488:web:8edcf67f6a06b1bdd8217d",
+  measurementId: "G-0824M3VMVY"
     });
